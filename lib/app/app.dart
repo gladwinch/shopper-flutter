2248512992abeb3305ee92669f6d73322b8c74c9
gladwinch <gladwinch20@gmail.com>
2023-04-28 07:18:43 +0530

    first commit

---
+++ lib/app/app.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:shopper/presentation/theme_manger.dart';
+
+class MyApp extends StatefulWidget {
+  const MyApp._internal();
+  static const MyApp instance = MyApp._internal();
+
+  factory MyApp() => instance;
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) { 
+    return MaterialApp(
+      theme: getApplicationTheme(),
+    );
+  }
+}
---
+++ lib/presentation/styles_manager.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'font_manager.dart';
+
+TextStyle _getTextStyle(
+  double fontSize,
+  String fontFamily,
+  Color color,
+  FontWeight fontWeight,
+) {
+  return TextStyle(
+    fontSize: fontSize,
+    fontFamily: fontFamily,
+    color: color,
+    fontWeight: fontWeight
+  );
+}
+
+// regular text style
+TextStyle getRegularStyle({
+  double fontSize = FontSize.s12,
+  required Color color
+}) {
+  return _getTextStyle(
+    fontSize, 
+    FontConstants.fontFamily,
+    color,
+    FontWeightManager.regular
+  );
+}
+
+// light text style
+TextStyle getLightStyle({
+  double fontSize = FontSize.s12,
+  required Color color
+}) {
+  return _getTextStyle(
+    fontSize, 
+    FontConstants.fontFamily,
+    color,
+    FontWeightManager.light
+  );
+}
+
+// bold text style
+TextStyle getBoldStyle({
+  double fontSize = FontSize.s12,
+  required Color color
+}) {
+  return _getTextStyle(
+    fontSize, 
+    FontConstants.fontFamily,
+    color,
+    FontWeightManager.bold
+  );
+}
+
+// semi bold text style
+TextStyle getSemiBoldStyle({
+  double fontSize = FontSize.s12,
+  required Color color
+}) {
+  return _getTextStyle(
+    fontSize, 
+    FontConstants.fontFamily,
+    color,
+    FontWeightManager.semiBold
+  );
+}
+
+// medium text style
+TextStyle getMediumStyle({
+  double fontSize = FontSize.s12,
+  required Color color
+}) {
+  return _getTextStyle(
+    fontSize, 
+    FontConstants.fontFamily,
+    color,
+    FontWeightManager.medium
+  );
+}
