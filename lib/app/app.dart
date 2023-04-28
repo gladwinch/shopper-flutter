fb3417a682403eba4cd60ed149cfd565f55598f4
gladwinch <gladwinch20@gmail.com>
2023-04-28 07:46:11 +0530

    route module update

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:shopper/presentation/theme_manger.dart';
+import 'package:shopper/presentation/resources/theme_manger.dart';
 
 class MyApp extends StatefulWidget {
   const MyApp._internal();
