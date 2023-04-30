225eb518d9a5d77db3b6f8127ba6c26e414d8538
gladwinch <gladwinch20@gmail.com>
2023-04-30 10:46:08 +0530

    splash screen

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:shopper/presentation/resources/theme_manger.dart';
+import '../presentation/resources/theme_manger.dart';
+import '../presentation/resources/routes_manager.dart';
 
 class MyApp extends StatefulWidget {
   const MyApp._internal();
@@ -15,6 +16,9 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) { 
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      onGenerateRoute: RouteGenerator.getRoute,
+      initialRoute: Routes.splashRoute,
       theme: getApplicationTheme(),
     );
   }
--- lib/presentation/splash/splash.dart
+++ lib/presentation/splash/splash.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import '../resources/assets_manager.dart';
+import '../resources/color_manager.dart';
 
 class SplashView extends StatefulWidget {
   const SplashView({super.key});
@@ -10,6 +12,9 @@ class SplashView extends StatefulWidget {
 class _SplashViewState extends State<SplashView> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: ColorManager.primary,
+      body: Center(child: Image(image: AssetImage(ImageAssets.splashLog)))
+    );
   }
 }
