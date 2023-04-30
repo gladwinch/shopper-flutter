05d0c10452beb9c9519bd96654b698c7ce0d5b2e
gladwinch <gladwinch20@gmail.com>
2023-04-30 11:20:48 +0530

    splash delay navigate to onboard

--- lib/presentation/onboarding/onboarding.dart
+++ lib/presentation/onboarding/onboarding.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../resources/color_manager.dart';
 
 class OnBoardingView extends StatefulWidget {
   const OnBoardingView({super.key});
@@ -10,6 +11,9 @@ class OnBoardingView extends StatefulWidget {
 class _OnBoardingViewState extends State<OnBoardingView> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: ColorManager.darkPrimary,
+      body: const Center(child: Text("Welcome to onboarding screen")),
+    );
   }
 }
--- lib/presentation/splash/splash.dart
+++ lib/presentation/splash/splash.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import '../resources/assets_manager.dart';
 import '../resources/color_manager.dart';
+import '../resources/routes_manager.dart';
 
 class SplashView extends StatefulWidget {
   const SplashView({super.key});
@@ -10,6 +11,25 @@ class SplashView extends StatefulWidget {
 }
 
 class _SplashViewState extends State<SplashView> {
+
+  void _navigateToNextScreen() async {
+    await Future.delayed(const Duration(seconds: 2));
+    _switchToOnboard();
+  }
+
+  void _switchToOnboard() {
+    Navigator.pushReplacementNamed(
+      context,
+      Routes.onBoardingRoute
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _navigateToNextScreen();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
